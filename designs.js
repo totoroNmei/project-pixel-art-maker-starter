60034fa6815d48989e8776b8b1ebb3deff094956
Qi Wu <wqi@umich.edu>
2018-02-04 20:22:18 -0800

    simple implementation

--- designs.js
+++ designs.js
@@ -1,10 +1,40 @@
-// Select color input
-// Select size input
+// global variable for canvas Panel
+const pixelPanel = $('#pixelCanvas');
 
-// When size is submitted by the user, call makeGrid()
+// when size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(makeGrid);
+// when a cell is clicked, fill its background with color in color picker
+$('#pixelCanvas').on('click', 'td', function(e) {
+	const color = $('#colorPicker').val();
+  $(this).css('background-color', color);
+}); 
+// clear canvas
+$('#clearCanvas').on('click', function() {
+	$('td').css('background-color', '#fff');
+})
 
-function makeGrid() {
+/*
+ * clear pixlePanel 
+ */
+function clearGrid() {
+  pixelPanel.empty();
+}
 
-// Your code goes here!
-
+/*
+ * create grid
+ */
+function makeGrid(e) {
+	e.preventDefault();
+	clearGrid();
+	// select size input
+	const width = parseInt($('#inputWeight').val());
+	const height = parseInt($('#inputHeight').val());
+	// nested for loop to create each cell
+  for (let row = 0; row < height; row++) {
+  	pixelPanel.append('<tr>'+row+'</tr>');
+  	const panelRow = pixelPanel.children().last();
+  	for (let col = 0; col < width; col++) {
+      panelRow.append('<td></td>');
+  	}
+  }
 }
